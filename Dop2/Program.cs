bb464cbc71aa0486356ac9091fb9e74025913096
Liubov <pandora1705@mail.ru>
2022-06-08 15:36:06 +0400

    Завершены все задачи

---
+++ Dop2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+
+Console.Write("Введите число: ");
+string numberstr = Console.ReadLine();
+double number = double.Parse(numberstr);
+if (number == 0)
+{
+    Console.Write("на ноль делить нельзя" );
+}
+else
+{
+    Console.Write($"Обратное значение числа {number} равно {1/number}" );
+}
