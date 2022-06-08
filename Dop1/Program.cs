5508e5bfae2a145e99abcedc66106d24863eb821
Liubov <pandora1705@mail.ru>
2022-06-08 15:14:02 +0400

    Завершена 1 доп задача

---
+++ Dop1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу вычисления модуля числа.
+
+Console.Write("Введите число: ");
+string numberstr = Console.ReadLine();
+int number = int.Parse(numberstr);
+
+if (number > 0)
+{
+    Console.Write($"Модуль числа {number} равен {number}" );
+}
+else
+{
+    Console.Write($"Модуль числа {number} равен {-number}" );
+}
