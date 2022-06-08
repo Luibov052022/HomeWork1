5a0011dea0e6642012522e579270a3b81de3b5e2
Liubov <pandora1705@mail.ru>
2022-06-08 14:35:43 +0400

    Ввод 3х чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+string numberstr1 = Console.ReadLine();
+int number1 = int.Parse(numberstr1);
+Console.Write("Введите второе число: ");
+string numberstr2 = Console.ReadLine();
+int number2 = int.Parse(numberstr2);
+Console.Write("Введите третье число: ");
+string numberstr3 = Console.ReadLine();
+int number3 = int.Parse(numberstr3);
